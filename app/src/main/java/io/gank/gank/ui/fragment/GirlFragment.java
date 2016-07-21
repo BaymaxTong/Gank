695a82b4d562bc3956673d3f74281ec8d0357f95
BaymaxTong <liurutong2012@gmail.com>
2016-07-21 14:31:54 +0800

    添加Realm缓存数据，还没有优化

--- app/src/main/java/io/gank/gank/ui/fragment/GirlFragment.java
+++ app/src/main/java/io/gank/gank/ui/fragment/GirlFragment.java
@@ -8,10 +8,12 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
@@ -26,6 +28,8 @@ import io.gank.gank.utils.NetWorkUtil;
 import io.gank.gank.utils.SnackBarUtil;
 import io.gank.gank.view.BatteryView;
 import io.gank.gank.view.HouseLoadingView;
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -44,6 +48,7 @@ public class GirlFragment extends Fragment {
     private int page = 1;
     private String title;
     private int lastVisibleItem ;
+    private Realm realm;
 
     public GirlFragment() {
         // Required empty public constructor
@@ -76,6 +81,18 @@ public class GirlFragment extends Fragment {
             public void onNext(List<Results> results) {
                 //更新数据
                 adapter.addDatas(results);
+                //缓存数据
+                realm = Realm.getDefaultInstance();
+                for(int i = 0;i < results.size();i++){
+                    RealmResults<Results> res = realm.where(Results.class).equalTo("desc",results.get(i).getDesc())
+                            .findAll();
+                    if (res == null||res.size() == 0) {
+                        realm.beginTransaction();
+                        Results realmRes = realm.copyToRealm(results.get(i));
+                        realm.commitTransaction();
+                    }
+                }
+                realm.close();
             }
         };
 
@@ -85,8 +102,11 @@ public class GirlFragment extends Fragment {
     }
 
     private void init() {
-        //首次请求数据
-        loadData(title, page, true);
+        initView();
+        initNet();
+    }
+
+    private void initView() {
         layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
@@ -125,13 +145,43 @@ public class GirlFragment extends Fragment {
         });
     }
 
+    private void initNet() {
+        //首次请求数据
+        loadData(title, page, false);
+    }
+
     //进行网络请求
     private void loadData(String type, int page, boolean refresh){
         if(!NetWorkUtil.isNetWorkConnected(getContext())){
-            SnackBarUtil.ShortSnackBar(recyclerView,"请连接网络... ( ＞ω＜)", SnackBarUtil.COLOR_PINK, SnackBarUtil.COLOR_GREEN).show();
+            getTagResult("福利");
+            SnackBarUtil.ShortSnackBar(recyclerView,"网络飞到外太空啦... ( ＞ω＜)", SnackBarUtil.COLOR_PINK, SnackBarUtil.COLOR_GREEN).show();
         }else {
             HttpMethods.getInstance().getGankData(new ProgressSubscriber(getDataOnNext, getActivity(), recyclerView, battery, refresh), type, page);
         }
+    }
+
+    public void getTagResult(String tag){
+        realm = Realm.getDefaultInstance();
+        List<Results> data = new ArrayList<>();
+        RealmResults<Results> result2 = realm.where(Results.class).equalTo("type",tag)
+                .findAll();
+        Log.d("debug",result2.toString());
+        for(int i = 0;i<result2.size();i++){
+            Results item = result2.get(i);
+            Results r = new Results();              //必须把查询到的数据转换一下不然报错。
+            r.set_id(item.get_id());
+            r.setCreatedAt(item.getCreatedAt());
+            r.setDesc(item.getDesc());
+            r.setPublishedAt(item.getPublishedAt());
+            r.setSource(item.getSource());
+            r.setType(item.getType());
+            r.setUrl(item.getUrl());
+            r.setWho(item.getWho());
+            r.setUsed(item.getUsed());
+            data.add(r);
+        }
+        adapter.addDatas(data);
+        realm.close();
     }
 
 }
