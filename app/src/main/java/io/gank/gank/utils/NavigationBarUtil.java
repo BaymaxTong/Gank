afe80b74f47876081865613334ac5cf88219a6ed
BaymaxTong <liurutong2012@gmail.com>
2016-08-02 09:45:20 +0800

    设配华为手机底部虚拟按键的问题

--- app/src/main/java/io/gank/gank/utils/NavigationBarUtil.java
+++ app/src/main/java/io/gank/gank/utils/NavigationBarUtil.java
@@ -3,6 +3,7 @@ package io.gank.gank.utils;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
+import android.util.Log;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.ViewConfiguration;
@@ -15,31 +16,12 @@ import java.lang.reflect.Method;
 public class NavigationBarUtil {
     // 判断设备是否有返回键、菜单键来确定是否有 NavigationBar
     public static boolean hasNavigationBar(Context context) {
-//        boolean hasMenuKey = ViewConfiguration.get(context).hasPermanentMenuKey();
-//        boolean hasBackKey = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_BACK);
-//        if (!hasMenuKey && !hasBackKey) {
-//            return true;
-//        }
-//        return false;
-        boolean hasNavigationBar = false;
-        Resources rs = context.getResources();
-        int id = rs.getIdentifier("config_showNavigationBar", "bool", "android");
-        if (id > 0) {
-            hasNavigationBar = rs.getBoolean(id);
+        boolean hasMenuKey = ViewConfiguration.get(context).hasPermanentMenuKey();
+        boolean hasBackKey = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_BACK);
+        if (!hasMenuKey) {// && !hasBackKey
+            return true;
         }
-        try {
-            Class systemPropertiesClass = Class.forName("android.os.SystemProperties");
-            Method m = systemPropertiesClass.getMethod("get", String.class);
-            String navBarOverride = (String) m.invoke(systemPropertiesClass, "qemu.hw.mainkeys");
-            if ("1".equals(navBarOverride)) {
-                hasNavigationBar = false;
-            } else if ("0".equals(navBarOverride)) {
-                hasNavigationBar = true;
-            }
-        } catch (Exception e) {
-
-        }
-        return hasNavigationBar;
+        return false;
     }
 
     // 获取 NavigationBar 的高度
